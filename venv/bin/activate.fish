b5158e167b87e61638e5261ab58d636aa036d2fd
Mahmoud-Mohamed-3 <mahmoudfalous@gmail.com>
2025-01-21 17:59:02 +0200

    fix_time_out_error

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/home/mano/Desktop/Python Scripts/venv'
+set -gx VIRTUAL_ENV '/home/mano/Desktop/Python Scripts/Get-Reviews-Script/venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
